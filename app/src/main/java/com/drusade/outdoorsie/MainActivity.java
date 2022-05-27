82f6e9d2a4494166a3b1f4181794d19ed487047f
DruSadeMumba <drusademumba@gmail.com>
2022-05-27 22:43:24 +0300

    Add onclick functionality to login button

--- app/src/main/java/com/drusade/outdoorsie/MainActivity.java
+++ app/src/main/java/com/drusade/outdoorsie/MainActivity.java
@@ -2,13 +2,42 @@ package com.drusade.outdoorsie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.loginButton) Button mLoginButton;
+
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.appNameText) TextView mAppNameText;
+
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.appSloganText) TextView mAppSloganText;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mLoginButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mLoginButton) {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }
     }
 }
