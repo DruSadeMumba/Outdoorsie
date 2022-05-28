c57de9e9b1fa1bc2d59f2d2f7ec8969b8204f919
DruSadeMumba <drusademumba@gmail.com>
2022-05-28 14:08:36 +0300

    Initialise bottom navigation bar and fragments

--- app/src/main/java/com/drusade/outdoorsie/ProfileActivity.java
+++ app/src/main/java/com/drusade/outdoorsie/ProfileActivity.java
@@ -4,7 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.drusade.outdoorsie.fragments.ActivitiesFragment;
+import com.drusade.outdoorsie.fragments.ExitFragment;
+import com.drusade.outdoorsie.fragments.MyProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class ProfileActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    ActivitiesFragment activitiesFragment = new ActivitiesFragment();
+    MyProfileFragment myProfileFragment = new MyProfileFragment();
+    ExitFragment exitFragment = new ExitFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
