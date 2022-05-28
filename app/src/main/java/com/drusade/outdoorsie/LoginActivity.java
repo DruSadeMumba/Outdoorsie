8aa165b92f1edb4d376e90d19557c00d5335101e
DruSadeMumba <drusademumba@gmail.com>
2022-05-28 12:03:27 +0300

    Add onclick listener for signin button

--- app/src/main/java/com/drusade/outdoorsie/LoginActivity.java
+++ app/src/main/java/com/drusade/outdoorsie/LoginActivity.java
@@ -2,13 +2,45 @@
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+  public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.password) EditText mPassword;
+
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.username) EditText mUsername;
+
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.signInButton) Button mSignInButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+        mSignInButton.setOnClickListener(this);
     }
-}
+
+      @Override
+      public void onClick(View v) {
+          if(v == mSignInButton) {
+              String username = mUsername.getText().toString();
+              String password = mPassword.getText().toString();
+              Intent intent = new Intent(LoginActivity.this, ProfileActivity.class);
+              intent.putExtra("username", username);
+              intent.putExtra("password", password);
+              startActivity(intent);
+          }
+      }
+  }
