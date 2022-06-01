374f19ca93d115cfed86729fdbb86a3618cef2c7
DruSadeMumba <drusademumba@gmail.com>
2022-06-01 13:06:08 +0300

    Add leave and stay buttons to exit fragment

--- app/src/main/java/com/drusade/outdoorsie/fragments/ExitFragment.java
+++ app/src/main/java/com/drusade/outdoorsie/fragments/ExitFragment.java
@@ -1,21 +1,63 @@
 package com.drusade.outdoorsie.fragments;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.drusade.outdoorsie.ActivitiesActivity;
+import com.drusade.outdoorsie.MainActivity;
 import com.drusade.outdoorsie.R;
 
-public class ExitFragment extends Fragment {
+import java.util.Objects;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class ExitFragment extends DialogFragment {
+
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.stay) Button stay;
+
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.leave) Button leave;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_exit, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ButterKnife.bind(this,view);
+
+        stay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requireActivity().finish();;
+            }
+        });
+
+        leave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requireActivity().moveTaskToBack(true);
+                android.os.Process.killProcess(android.os.Process.myPid());
+                System.exit(1);
+            }
+        });
+
+
     }
 }
