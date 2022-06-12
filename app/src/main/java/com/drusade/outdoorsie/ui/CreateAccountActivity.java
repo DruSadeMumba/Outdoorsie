c59f2f98265a37680736065e09ae22da5bd14f37
DruSadeMumba <drusademumba@gmail.com>
2022-06-12 20:09:39 +0300

    Add create accout activity

---
+++ app/src/main/java/com/drusade/outdoorsie/ui/CreateAccountActivity.java
@@ -0,0 +1,16 @@
+package com.drusade.outdoorsie.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.drusade.outdoorsie.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
