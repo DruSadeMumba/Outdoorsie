c20a3c178f90666f20d103c11d444da86aceed52
DruSadeMumba <drusademumba@gmail.com>
2022-06-13 12:17:16 +0300

    Create an activity model class

---
+++ app/src/main/java/com/drusade/outdoorsie/models/AnActivity.java
@@ -0,0 +1,25 @@
+package com.drusade.outdoorsie.models;
+
+public class AnActivity {
+    private String activityName;
+    private String location;
+    private long date;
+
+    public AnActivity() {
+    }
+
+    public AnActivity(String activityName, String location) {
+        this.activityName = activityName;
+        this.location = location;
+    }
+
+    //Getters
+    public String getActivityName() {return activityName;}
+    public String getLocation() {return location;}
+    public long getDate() {return date;}
+
+    //Setters
+    public void setActivityName(String activityName) {this.activityName = activityName;}
+    public void setLocation(String location) {this.location = location;}
+    public void setDate(long date) {this.date = date;}
+}
